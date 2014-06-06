ba3b0a7a81dcfb4c4a6f742eee3dd51c00337372
Jeff Felchner <accounts+git@thekompanee.com>
2014-06-05 19:48:58 -0500

    Bugfix: Make find a class method instead of an instance method
    When we call the standard `ActiveRecord.find` it's on the class, not the
    instance so `Persistable` needs to reflect that fact.
    
    --------------------------------------------------------------------------------
    Change-Id:     I78d2c478429dde615b154a5e38dacf6b96d5ef46
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- lib/human_error/persistable.rb
+++ lib/human_error/persistable.rb
@@ -1,5 +1,24 @@
 class   HumanError
 module  Persistable
+  module ClassMethods
+    def find(*ids)
+      super
+    rescue ActiveRecord::RecordNotFound => e
+      ids = case e.message
+            when /\ACouldn't find .* without an ID\z/
+              []
+            when /\ACouldn't find .* with \'.*\'=(\d+)\z/
+              [$1]
+            when /\ACouldn't find all .* with \'.*\': ((?:\d+(?:, )?)+)\z/
+              $1.split(', ')
+            end
+
+      raise HumanError::Errors::ResourceNotFoundError.new(
+        resource_name:    human_error_resource_name(self),
+        resource_id:      ids)
+    end
+  end
+
   def save!(*args)
     super
   rescue ActiveRecord::InvalidForeignKey => e
@@ -9,43 +28,31 @@ module  Persistable
                                          [1..-1]
 
     raise HumanError::Errors::AssociationError.new(
-      resource_name:    human_error_resource_name,
+      resource_name:    human_error_resource_name(self.class),
       association_name: association_name,
       association_id:   association_id,
       attributes:       attributes)
   rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved => e
     raise HumanError::Errors::ResourcePersistenceError.new(
-      resource_name:    human_error_resource_name,
+      resource_name:    human_error_resource_name(self.class),
       attributes:       attributes,
       errors:           errors.full_messages)
   end
 
-  def find(*ids)
-    super
-  rescue ActiveRecord::RecordNotFound => e
-    ids = case e.message
-          when /\ACouldn't find .* without an ID\z/
-            []
-          when /\ACouldn't find .* with \'.*\'=(\d+)\z/
-            [$1]
-          when /\ACouldn't find all .* with \'.*\': ((?:\d+(?:, )?)+)\z/
-            $1.split(', ')
-          end
-
-    raise HumanError::Errors::ResourceNotFoundError.new(
-      resource_name:    human_error_resource_name,
-      resource_id:      ids)
-  end
-
-  def human_error_resource_name
+  def human_error_resource_name(klass)
     last_part_of_class_name = /::(\w+)\z/
 
-    self.
-      class.
+    klass.
       name[last_part_of_class_name, 1].
       underscore.
       humanize.
       downcase
+  end
+
+  module_function :human_error_resource_name
+
+  def self.included(base)
+    base.extend ClassMethods
   end
 end
 end
