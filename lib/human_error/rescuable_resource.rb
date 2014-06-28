8093618dc665532c9b1eb8f2003650f41b9969da
Jeff Felchner <accounts+git@thekompanee.com>
2014-06-28 01:55:46 -0500

    Chore: Move RescuableResource from Apill to HumanError
    See this commit for more details:
    
    https://github.com/thekompanee/apill/commit/1975a5e215c6247464f71f65d9489cb2db5d5c24
    
    --------------------------------------------------------------------------------
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

---
+++ lib/human_error/rescuable_resource.rb
@@ -0,0 +1,58 @@
+require 'human_error/errors/crud_errors/resource_persistence_error'
+require 'human_error/errors/crud_errors/resource_not_found_error'
+require 'human_error/errors/crud_errors/association_error'
+
+class   HumanError
+module  RescuableResource
+  module ClassMethods
+    def rescue_resource(resource_name, from:, via:)
+      lookup_library = via
+
+      if from.include? 'persistence'
+        rescue_from HumanError::Errors::ResourcePersistenceError do |e|
+          error = lookup_library.fetch(
+                  'ResourcePersistenceError',
+                  resource_name: e.resource_name,
+                  attributes:    e.attributes,
+                  errors:        e.errors,
+                  action:        action_name)
+
+          render json:   error,
+                 status: error.http_status
+        end
+      end
+
+      if from.include? 'not_found'
+        rescue_from HumanError::Errors::ResourceNotFoundError do |e|
+          error = lookup_library.fetch(
+                  'ResourceNotFoundError',
+                  resource_name: e.resource_name,
+                  resource_id:   e.resource_id,
+                  action:        action_name)
+
+          render json:   error,
+                 status: error.http_status
+        end
+      end
+
+      if from.include? 'association'
+        rescue_from HumanError::Errors::AssociationError do |e|
+          error = lookup_library.fetch(
+                  'AssociationError',
+                  resource_name:    e.resource_name,
+                  association_name: e.association_name,
+                  association_id:   e.association_id,
+                  attributes:       e.attributes)
+
+          render json:   error,
+                 status: error.http_status
+        end
+      end
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+end
+end
---
+++ spec/lib/human_error/rescuable_resource_spec.rb
@@ -0,0 +1,7 @@
+require 'rspectacular'
+require 'human_error/rescuable_resource'
+
+class     HumanError
+describe  RescuableResource do
+end
+end
