2798a969131bb40d3055c75a082f3f609993033b
Jeff Felchner <accounts+git@thekompanee.com>
2015-04-10 22:48:31 -0500

    Changed: VerifiableModel to allow the lookup library to be passed in
    --------------------------------------------------------------------------------
    Branch:        refactor/stop-monkeypatching-activerecord-objects
    Change-Id:     I60dc694f53839d7c156db0dfafdfa26dd061f159
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- lib/human_error/verifiable_model.rb
+++ lib/human_error/verifiable_model.rb
@@ -3,7 +3,9 @@ require 'human_error/errors/crud_errors/resource_not_found_error'
 class   HumanError
 module  VerifiableModel
   module ClassMethods
+    def verify_model_exists(model_name = nil, options = {}, via: HumanError.new)
       exceptions     = options[:except] || %i{create index}
+      lookup_library = via
       model_name   ||= name[/::(\w+)Controller\z/, 1].
                          singularize.
                          downcase
@@ -11,7 +13,8 @@ module  VerifiableModel
       before_action except: exceptions do
         model = public_send(model_name)
 
-        resource_not_found_error = Errors::ResourceNotFoundError.new(
+        resource_not_found_error = lookup_library.build(
+          'ResourceNotFoundError',
           resource_name: model_name,
           resource_id:   params[:id],
         )
