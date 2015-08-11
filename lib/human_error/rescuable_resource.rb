988c578a8bb6c5f34b8427d0e42202ef5c8342c2
Jeff Felchner <accounts+git@thekompanee.com>
2015-08-11 02:46:28 -0500

    Removed: Unneeded requires

--- lib/human_error/rescuable_resource.rb
+++ lib/human_error/rescuable_resource.rb
@@ -1,7 +1,3 @@
-require 'human_error/errors/crud_errors/resource_persistence_error'
-require 'human_error/errors/crud_errors/resource_not_found_error'
-require 'human_error/errors/crud_errors/association_error'
-
 class   HumanError
 module  RescuableResource
   module ClassMethods
